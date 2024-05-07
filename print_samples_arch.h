3c04eda598336c5d70dcd4b6fc3ce9a62b53c7c9
MadMuses <mamusique128@gmail.com>
2024-05-07 16:41:42 +0200

    Created this file for easier managment

---
+++ print_samples_arch.h
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdlib>
+
+#ifndef print_samples_arch_h
+#define print_samples_arch_h
+#endif
+
+// Include the Faust DSP (and other) classes
+#include "./faust/dsp/dsp.h"
+#include "./faust/gui/UI.h"
+#include "./faust/gui/UI.h"
+#include "./faust/audio/dummy-audio.h"
+
+// PrintSamplesAdapter class
+class PrintSamplesAdapter : public dsp {
+public:
+    dsp* fDSP;
+
+    PrintSamplesAdapter(dsp* dsp) : fDSP(dsp) {}
+
+    virtual int getNumInputs() { return fDSP->getNumInputs(); }
+    virtual int getNumOutputs() { return fDSP->getNumOutputs(); }
+    virtual void buildUserInterface(UI* ui_interface) { fDSP->buildUserInterface(ui_interface); }
+    virtual int getSampleRate() { return fDSP->getSampleRate(); }
+    virtual void init(int sample_rate) { fDSP->init(sample_rate); }
+    virtual void instanceInit(int sample_rate) { fDSP->instanceInit(sample_rate); }
+    virtual void instanceConstants(int sample_rate) { fDSP->instanceConstants(sample_rate); }
+    virtual void instanceResetUserInterface() { fDSP->instanceResetUserInterface(); }
+    virtual void instanceClear() { fDSP->instanceClear(); }
+    virtual void metadata(Meta* m) {fDSP->metadata(m);}
+    virtual dsp* clone() { return new PrintSamplesAdapter(fDSP->clone()); }
+
+    // Override the compute method to print samples
+    virtual void compute(int count, FAUSTFLOAT** inputs, FAUSTFLOAT** outputs) {
+        fDSP->compute(count, inputs, outputs);
+        for (int i = 0; i < count; ++i) {
+            for (int j = 0; j < getNumOutputs(); ++j) {
+                std::cout << "Sample[" << i << "][" << j << "]: " << outputs[j][i] << std::endl;
+            }
+        }
+    }
+};
