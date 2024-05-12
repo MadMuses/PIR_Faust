958e423718606d0c56b1645004e63b6465173314
MadMuses <62542620+MadMuses@users.noreply.github.com>
2024-05-12 22:26:25 +0200

    Reorganised .h file

--- print_samples_arch.h
+++ print_samples_arch.h
@@ -12,31 +12,34 @@
 #include "./faust/audio/dummy-audio.h"
 
 // PrintSamplesAdapter class
-class PrintSamplesAdapter : public dsp {
-public:
-    dsp* fDSP;
+class PrintSamplesAdapter : public dsp 
+{
+    public:
+        dsp* fDSP;
 
-    PrintSamplesAdapter(dsp* dsp) : fDSP(dsp) {}
+        PrintSamplesAdapter(dsp* dsp) : fDSP(dsp) {}
+        virtual dsp* clone() { return new PrintSamplesAdapter(fDSP->clone()); }
 
-    virtual int getNumInputs() { return fDSP->getNumInputs(); }
-    virtual int getNumOutputs() { return fDSP->getNumOutputs(); }
-    virtual void buildUserInterface(UI* ui_interface) { fDSP->buildUserInterface(ui_interface); }
-    virtual int getSampleRate() { return fDSP->getSampleRate(); }
-    virtual void init(int sample_rate) { fDSP->init(sample_rate); }
-    virtual void instanceInit(int sample_rate) { fDSP->instanceInit(sample_rate); }
-    virtual void instanceConstants(int sample_rate) { fDSP->instanceConstants(sample_rate); }
-    virtual void instanceResetUserInterface() { fDSP->instanceResetUserInterface(); }
-    virtual void instanceClear() { fDSP->instanceClear(); }
-    virtual void metadata(Meta* m) {fDSP->metadata(m);}
-    virtual dsp* clone() { return new PrintSamplesAdapter(fDSP->clone()); }
+        virtual int getNumInputs() { return fDSP->getNumInputs(); }
+        virtual int getNumOutputs() { return fDSP->getNumOutputs(); }
+        virtual void buildUserInterface(UI* ui_interface) { fDSP->buildUserInterface(ui_interface); }
+        virtual int getSampleRate() { return fDSP->getSampleRate(); }
+        virtual void init(int sample_rate) { fDSP->init(sample_rate); }
+        virtual void instanceInit(int sample_rate) { fDSP->instanceInit(sample_rate); }
+        virtual void instanceConstants(int sample_rate) { fDSP->instanceConstants(sample_rate); }
+        virtual void instanceResetUserInterface() { fDSP->instanceResetUserInterface(); }
+        virtual void instanceClear() { fDSP->instanceClear(); }
+        virtual void metadata(Meta* m) {fDSP->metadata(m);}
 
-    // Override the compute method to print samples
-    virtual void compute(int count, FAUSTFLOAT** inputs, FAUSTFLOAT** outputs) {
-        fDSP->compute(count, inputs, outputs);
-        for (int i = 0; i < count; ++i) {
-            for (int j = 0; j < getNumOutputs(); ++j) {
+        // Redefine compute
+        void compute(int count, FAUSTFLOAT** inputs, FAUSTFLOAT** outputs){
+            fDSP->compute(count, inputs, outputs);
+            for (int i = 0; i < count; ++i) {
+                for (int j = 0; j < getNumOutputs(); ++j) 
+                {
                 std::cout << "Sample[" << i << "][" << j << "]: " << outputs[j][i] << std::endl;
+                }
             }
-        }
-    }
+        };
+        
 };
